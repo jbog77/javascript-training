567c30c61db736948fc9fc5404c9b77330016b9f
jbog77 <bogota.jeremy@gmail.com>
2017-03-16 09:40:35 +0100

    git status

--- src/functions/a-operation.js
+++ src/functions/a-operation.js
@@ -1,18 +1,26 @@
 // add, minus, div, mult opeations
 
-function print(){
-    
+// Standard base
+function add(x, y) {
+    return x + y;
 }
 
-const minus = function(){
-    
-    
+// Functions are variables
+const minus = function (x, y) {
+    return x - y;
 }
 
-const div = (x,y) => x/y;
+// One liner FAT arrows (ES 2015)
+const div = (x, y) => x / y; // no return needed
 
+// Multiline
+const mult = (x, y) => {//multiline fat arrox need {}
+    console.log('mult', x, 'by', y);
+    return x * y; // return NEED for multiline fat arrow
+}
 
-
+l=console.log;
+l(add(2,4));
 
 
 
--- src/start.js
+++ src/start.js
@@ -1,2 +1,9 @@
-var x=2+2;
+const x = 2 + 2;
 console.log(x);
+
+for (var i = 0; i < 10; i++) {
+    let x = i + 1;
+    console.log(x);
+}
+
+console.log("final x:", x)
